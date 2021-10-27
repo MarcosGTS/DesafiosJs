b9ee2470c624d57eadd2ca7c9ec98aad82d2c721
MarcosGTS <marcosgtsodre@gmail.com>
2021-10-27 07:11:03 -0300

    adicao wrap DOGO

---
+++ Dogo/cats.js
@@ -0,0 +1,48 @@
+const cats = [
+    {
+       name: 'Snuggles',
+       credit: "https://images.dog.ceo/breeds/terrier-scottish/n02097298_3589.jpg"
+   },
+    {
+       name: 'Cruella',
+       credit: "https://images.dog.ceo/breeds/terrier-american/n02093428_225.jpg"
+   },
+   {
+       name: "Ruffus",
+       credit:'https://images.dog.ceo/breeds/frise-bichon/3.jpg'
+   },
+   {
+      name: 'Cruella',
+      credit: "https://images.dog.ceo/breeds/terrier-american/n02093428_225.jpg"
+  },
+  {
+      name: "Ruffus",
+      credit:'https://images.dog.ceo/breeds/frise-bichon/3.jpg'
+  },
+  {
+     name: 'Cruella',
+     credit: "https://images.dog.ceo/breeds/terrier-american/n02093428_225.jpg"
+ },
+ {
+     name: "Ruffus",
+     credit:'https://images.dog.ceo/breeds/frise-bichon/3.jpg'
+ },
+ {
+    name: 'Cruella',
+    credit: "https://images.dog.ceo/breeds/terrier-american/n02093428_225.jpg"
+},
+{
+    name: "Ruffus",
+    credit:'https://images.dog.ceo/breeds/frise-bichon/3.jpg'
+},
+{
+   name: 'Cruella',
+   credit: "https://images.dog.ceo/breeds/terrier-american/n02093428_225.jpg"
+},
+{
+   name: "Ruffus",
+   credit:'https://images.dog.ceo/breeds/frise-bichon/3.jpg'
+}
+]
+
+export default cats
--- Dogo/script.js
+++ Dogo/script.js
@@ -1,21 +1,9 @@
+import cats from "./cats.js"
+
 document.addEventListener("DOMContentLoaded", () => {
-    const cats = [
-         {
-            name: 'Snuggles',
-            credit: "https://images.dog.ceo/breeds/terrier-scottish/n02097298_3589.jpg"
-        },
-         {
-            name: 'Cruella',
-            credit: "https://images.dog.ceo/breeds/terrier-american/n02093428_225.jpg"
-        },
-        {
-            name: "Ruffus",
-            credit:'https://images.dog.ceo/breeds/frise-bichon/3.jpg'
-        }
-    ]
-
+    
     const usersList = document.querySelector("#usersList")
-
+    console.log(cats)
     cats.forEach(cat => {
         const node = `<div class="user">
                         <img src="${cat.credit}" alt="problema">
