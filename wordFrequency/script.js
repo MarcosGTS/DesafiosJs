5efe91722d4b605a2641fad6c0b68f166ede421c
MarcosGTS <marcosgtsodre@gmail.com>
2021-11-04 10:13:59 -0300

    adicionando style

--- wordFrequency/script.js
+++ wordFrequency/script.js
@@ -1,41 +1,65 @@
 document.addEventListener("DOMContentLoaded", main);
 
-function main() {
+function main() 
+{
     let countBtn = document.querySelector("#countButton")
 
     countBtn.addEventListener("click", () => {
         let textValue = document.querySelector("#textInput").value
+
         let wordsList = createListWords(textValue)
         displayWordList(wordsList)
+
     })
 }
 
-function createListWords (text) {
+function createListWords (text) 
+{
     let words = {}
 
     //Split elements when find a char != alphanum
+    //Adding and count existent words
+
     text.split(/\W+/).filter(s => s)
-        //Adding and count existent words
         .forEach(word => {
-            if (!words[word])
-                words[word] = 1
-            else
-                words[word] += 1
+            word = word.toLowerCase()
+
+            words[word] = (words[word] += 1) || 1
         })
-    
-    return words
+
+    return objToList(words)
 }
 
 function displayWordList(wordsList)
 {
     let display = document.querySelector("#list")
     
-    display.innerHTML = ""
+    display.innerHTML = `
+    <thead>
+    <tr>
+        <td>Word</td>
+        <td>Frequency</td>
+    </tr>
+    </thead>`
     
-    for (let key in wordsList){
+    wordsList.forEach( obj => {
         display.innerHTML += "<tr>"
-        display.innerHTML += `<td>${key}</td><td>${wordsList[key]}</td>`
+        display.innerHTML += `<td>${obj.word}</td><td>${obj.frequency}</td>`
         display.innerHTML += "</tr>"
-    }
+    })
 
+    if (wordsList.length == 0)
+        display.innerHTML = "No words"
+        
+}
+
+function objToList(obj)
+{
+    let list = []
+
+    for (let key in obj){
+        list.push({word: key, frequency: obj[key]})
+    }
+    
+    return list.sort((a, b) => b.frequency - a.frequency)
 }
