6c787f5881c65851b7cd59e1de71ffb940fa8cf6
MarcosGTS <marcosgtsodre@gmail.com>
2021-11-07 05:44:30 -0300

    adicao do Spawn de inimigos

--- canvasGame/script.js
+++ canvasGame/script.js
@@ -5,6 +5,7 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 let BULLETS = []
+let ENEMIES = []
 
 class Player {
     constructor(x, y, radius, color)
@@ -52,8 +53,39 @@ class Bullet {
     { 
         this.x += this.velocity * Math.cos(this.angle)
         this.y += this.velocity * Math.sin(this.angle)
+        this.draw()
     }
 }
+
+class Enemy {
+    constructor(x, y, radius, color, velocity, directionX, directionY)
+    {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+        this.directionX = directionX
+        this.directionY = directionY
+    }
+
+    draw()
+    {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update()
+    { 
+        let angle = Math.atan2(this.directionY - this.y,this.directionX - this.x)
+        this.x += this.velocity * Math.cos(angle)
+        this.y += this.velocity * Math.sin(angle)
+        this.draw()
+    }
+}
+
 
 let cCenterX = canvas.width / 2
 let cCenterY = canvas.height / 2
@@ -64,14 +96,36 @@ window.addEventListener("click", (e) => {
     player.shoot(e.clientX, e.clientY)
 })
 
+function spawnEnemy() {
+    setInterval(() => {
+        let radius = 30
+        let x
+        let y
+        if (Math.random() < 0.5)
+        {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+        }
+        else 
+        {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+        
+        let color = "green"
+
+        ENEMIES.push(new Enemy(x, y, radius, color, 3,player.x, player.y))
+    }, 200);
+}
+
+spawnEnemy()
+
 function animate()
 {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
-    BULLETS.forEach(b => {
-        b.update()
-        b.draw()
-    })
+    BULLETS.forEach(b => b.update())
+    ENEMIES.forEach(e => e.update())
     player.draw()
 }
 
