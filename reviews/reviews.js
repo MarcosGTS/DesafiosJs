8e3d3d13d1a55607d69d5931bbfa9d0c1db8285d
MarcosGTS <marcosgtsodre@gmail.com>
2021-11-13 06:32:20 -0300

    mudancas imgs

--- reviews/reviews.js
+++ reviews/reviews.js
@@ -1,22 +1,18 @@
 const data = [
     {
-        img: './personImgs/person0.jpg',
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem reprehenderit voluptatibus autem nesciunt, magni aspernatur nostrum omnis voluptatum similique unde facilis, eligendi ex atque. Deserunt reprehenderit perferendis esse excepturi doloribus."
     },
     {
-        img: './personImgs/person1.jpg',
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem reprehenderit voluptatibus autem nesciunt, magni aspernatur nostrum omnis voluptatum similique unde facilis, eligendi ex atque. Deserunt reprehenderit perferendis esse excepturi doloribus."
     },
     {
-        img: './personImgs/person2.jpg',
+        img:  "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem reprehenderit voluptatibus autem nesciunt, magni aspernatur nostrum omnis voluptatum similique unde facilis, eligendi ex atque. Deserunt reprehenderit perferendis esse excepturi doloribus."
     },
     {
-        img: './personImgs/person3.jpg',
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem reprehenderit voluptatibus autem nesciunt, magni aspernatur nostrum omnis voluptatum similique unde facilis, eligendi ex atque. Deserunt reprehenderit perferendis esse excepturi doloribus."
-    },
-    {
-        img: './personImgs/person4.jpg',
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem reprehenderit voluptatibus autem nesciunt, magni aspernatur nostrum omnis voluptatum similique unde facilis, eligendi ex atque. Deserunt reprehenderit perferendis esse excepturi doloribus."
     }
 ]
