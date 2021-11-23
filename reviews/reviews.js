f922eafc05a098ad75e3f38b4f4567badf780444
MarcosGTS <marcosgtsodre@gmail.com>
2021-11-23 07:54:19 -0300

    atualizacao do estilo do desafio Reviews

--- reviews/reviews.js
+++ reviews/reviews.js
@@ -1,19 +1,19 @@
 const data = [
     {
         img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem reprehenderit voluptatibus autem nesciunt, magni aspernatur nostrum omnis voluptatum similique unde facilis, eligendi ex atque. Deserunt reprehenderit perferendis esse excepturi doloribus."
+        description: "Nunc at velit id neque accumsan laoreet eu at justo. Etiam convallis fringilla laoreet. In rhoncus mattis lorem dapibus condimentum. Vivamus aliquet, nisl id placerat molestie, turpis purus finibus sapien, vel mattis lacus arcu vitae elit. Nulla facilisi"
     },
     {
         img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem reprehenderit voluptatibus autem nesciunt, magni aspernatur nostrum omnis voluptatum similique unde facilis, eligendi ex atque. Deserunt reprehenderit perferendis esse excepturi doloribus."
+        description: "Phasellus quis venenatis libero. Mauris eu magna ornare, placerat sapien non, egestas tortor. Phasellus feugiat, mi consequat hendrerit dapibus, mauris justo congue felis, et mattis ante massa ac risus. Vivamus auctor imperdiet metus. Quisque viverra ultricies posuere."
     },
     {
         img:  "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem reprehenderit voluptatibus autem nesciunt, magni aspernatur nostrum omnis voluptatum similique unde facilis, eligendi ex atque. Deserunt reprehenderit perferendis esse excepturi doloribus."
+        description: "Sed dictum magna sed maximus tempor. Nullam molestie magna eget porttitor rhoncus. Proin viverra sed purus eget ornare. Nam placerat nisl accumsan tortor malesuada dignissim eget quis justo. Pellentesque lobortis metus id magna cursus, sit amet accumsan nunc sodales."
     },
     {
         img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem reprehenderit voluptatibus autem nesciunt, magni aspernatur nostrum omnis voluptatum similique unde facilis, eligendi ex atque. Deserunt reprehenderit perferendis esse excepturi doloribus."
+        description: "Morbi suscipit congue nisl facilisis posuere. Donec ut bibendum dui. Pellentesque pretium lacinia lorem, eget commodo dolor eleifend at. Praesent congue nisl neque. Pellentesque consectetur nisi nulla, ac hendrerit nisl sagittis at."
     }
 ]
 
